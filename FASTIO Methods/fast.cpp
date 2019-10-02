2cb5dd43156de936421259360572c96d3bb1cc6e
Dcoder-Dev <devangdwivedi2001@gmail.com>
2019-10-02 14:48:44 +0530

    Update fast.cpp with scanf and printf

--- FASTIO Methods/fast.cpp
+++ FASTIO Methods/fast.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
   long long a;
-  cin>>a;
-  cout<<a;
+  scanf("%d", a);
+  printf("%d", a);
   return 0;
 }
+
+//scanf and printf are much faster input methods compared to cin and cout
